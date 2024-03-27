5635429891a90e209a3e56a3f87df6b7cc2e8d59
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-03-27 11:26:59 +0000

    Update PonyPlace CDN url

--- src/loader.ts
+++ src/loader.ts
@@ -13,7 +13,7 @@
 import {constants} from "./constants";
 import {gm_fetch, headerStringToObject} from "./utils";
 
-const url = "https://ponyplace-cdn.equestria.horse/minimap.user.js";
+const url = "https://ponyplace-cdn.equestria.horse/ponyplace/minimap.user.js";
 
 (async function () {
   let etag: string | undefined;
