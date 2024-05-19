7af7af21378c690989aecd80ef3b48ce465b8a7d
CloudburstSys <43821940+CloudburstSys@users.noreply.github.com>
2024-05-19 18:17:20 +0100

    Change database setup

--- analytics/dbsetup.sql
+++ analytics/dbsetup.sql
@@ -1,31 +1,51 @@
 -- intended to be sourced into psql, after changing the passwords
-CREATE USER writer PASSWORD 'lol';
-CREATE USER grafana PASSWORD 'no';
+CREATE USER api PASSWORD 'password';
+CREATE USER grafana PASSWORD 'password';
 
-CREATE DATABASE ponyplace_analytics;
-GRANT ALL ON DATABASE ponyplace_analytics TO writer;
-GRANT CONNECT ON DATABASE ponyplace_analytics TO grafana;
+CREATE DATABASE analytics;
+GRANT ALL ON DATABASE analytics TO api;
+GRANT CONNECT ON DATABASE analytics TO grafana;
 
 \c ponyplace_analytics
 
 CREATE TABLE placements(
         timestamp timestamp,
+        user varchar(36),
         template varchar(40),
-        json text
+        x int,
+        y int,
+        color int,
+        next_pixel timestamp
 );
 
 CREATE INDEX bytime ON placements(timestamp, template);
 
 GRANT SELECT ON placements TO grafana;
-GRANT INSERT ON placements TO writer;
+GRANT SELECT ON placements TO api;
+GRANT INSERT ON placements TO api;
 
-CREATE TABLE others(
+CREATE TABLE errors(
         timestamp timestamp,
-        event varchar(40),
-        json text
-);
+        user varchar(36),
+        message text
+)
 
-CREATE INDEX others_bytime ON others(timestamp);
+CREATE INDEX bytime ON errors(timestamp, template);
 
-GRANT SELECT ON others TO grafana;
-GRANT INSERT ON others TO writer;
+GRANT SELECT ON errors TO grafana;
+GRANT SELECT ON errors TO api;
+GRANT INSERT ON errors TO api;
+
+CREATE TABLE completion_status(
+        timestamp timestamp,
+        user varchar(36),
+        template varchar(40),
+        correct int,
+        total int
+)
+
+CREATE INDEX bytime ON completion_status(timestamp, template);
+
+GRANT SELECT ON completion_status TO grafana;
+GRANT SELECT ON completion_status TO api;
+GRANT INSERT ON completion_status TO api;
